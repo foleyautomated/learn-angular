4cc3902b0109532a4faff0796619fb4dcfb19165
David Foley <foleyautomated@gmail.com>
2024-07-03 13:51:17 -0400

    28-ClickToChangeUser

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , signal} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,18 +11,18 @@ let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
   clickCounter = 1;
 
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.selectedUser().avatar;
   }
 
   onSelectUser() {
     console.log(`Clicked ${this.clickCounter} times! New User ${this.selectedUser.name}`)
     this.clickCounter+=1;
     randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex]
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
   }
 }
